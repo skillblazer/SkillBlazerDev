741be8e9cff274eaac46c5fd78beae75e22d9050
rebec <rebec@REBECCA-LAPTOP>
2018-04-06 12:50:09 -0400

    Added back currentStreak and bestStreak fields and getter methods associated with them (i.e. getCurrentStreak, getBestStreak).

--- SkillBlazerDev/src/llamasoft/skillblazer/DailyTask.java
+++ SkillBlazerDev/src/llamasoft/skillblazer/DailyTask.java
@@ -6,22 +6,22 @@ import java.util.Calendar;
  * The child classes listed above represent the different types of goals that
  * define these habits/skills.
  *
- * A daily task is one in which the user has the
- * goal of completing every day, such as exercising for 1 hour.
+ * A daily task is one in which the user has the goal of completing every day,
+ * such as exercising for 1 hour.
  *
  * The DailyTask class contains fields and methods associated with the
- * completion of a task on a daily basis.
- * It includes fields for current streak and best streak and
- * methods to set and get these values. It also contains a method to set a task
- * as being completed for a given day and a method to get that value.
+ * completion of a task on a daily basis. It includes fields for current streak
+ * and best streak and methods to set and get these values. It also contains a
+ * method to set a task as being completed for a given day and a method to get
+ * that value.
  */
-
 public class DailyTask extends Task {
 
     /* REMEMBER ALL YOUR SUPERCLASS MEMBERS!!! */
-
     private boolean isCompleted = false;
     private String taskName;
+    private int currentStreak;
+    private int bestStreak;
 
     public boolean getIsCompleted() {
         return isCompleted;
@@ -33,6 +33,14 @@ public class DailyTask extends Task {
 
     public String getTaskName() {
         return this.taskName;
+    }
+
+    public int getCurrentStreak() {
+        return this.currentStreak;
+    }
+
+    public int getBestStreak() {
+        return this.bestStreak;
     }
 
     public DailyTask() {
