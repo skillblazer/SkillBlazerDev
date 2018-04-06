4fc0e6989d71f3f868b7410f59d88b12e56c194b
Jason Engel <jason-engel@hotmail.com>
2018-04-06 22:36:19 +0900

    Added boolean values and setters to DropBoxIntegration.java and TwitterIntegration.java

--- SkillBlazerDev/src/llamasoft/skillblazer/TwitterIntegration.java
+++ SkillBlazerDev/src/llamasoft/skillblazer/TwitterIntegration.java
@@ -1,4 +1,10 @@
 package llamasoft.skillblazer;
 
 public class TwitterIntegration {
+
+    private boolean isEnabled;
+
+    public void enableTwitterIntegration(Boolean isEnabled) {
+        this.isEnabled = isEnabled;
+    }
 }
