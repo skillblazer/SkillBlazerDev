4b6ec3f1a6e9b0a539d36d1d318b63f781189dbd
luke <luke@gmail.com>
2018-04-17 02:23:20 -0400

    Merge origin/PhaseII_RDavis_20180416 into luke_phaseII
    Conflicts:
    	SkillBlazerDev/src/llamasoft/skillblazer/Task.java

--- SkillBlazerDev/src/llamasoft/skillblazer/Task.java
+++ SkillBlazerDev/src/llamasoft/skillblazer/Task.java
@@ -4,7 +4,7 @@ package llamasoft.skillblazer;
  * The Task class contains the methods and fields that will apply to all of
  * the types of tasks. Included in the Task class will be methods to initialize
  * the task, edit the task, delete the task, get task ID, get start date, and
- * get task name.
+ * get task name.  
  */
 
 import java.util.Calendar;
